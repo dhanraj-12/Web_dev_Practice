46c973b4220adb2939e44f2461b13631f693e01d
Dhanraj Jadhav <dj004786@gmail.com>
2024-12-11 23:54:37 +0530

    tailwind added

---
+++ react_learn/tailwindreact/src/component/Card.jsx
@@ -0,0 +1,39 @@
+import cat from '/home/dj/drive/Web_Dev/react_learn/tailwindreact/src/assets/cat.jpeg';
+
+function Card(props) {
+
+    return(
+        <>
+          <div className="flex flex-col rounded-xl  p-4"
+        style={{
+          border: '0.88px solid',
+  
+          backdropFilter: 'saturate(180%) blur(14px)',
+          background: ' #ffffff0d',
+        }}
+      >
+        <div>
+          <img
+            src= {cat}
+            alt="nft-gif"
+            width="400"
+            height="400"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="flex flex-col  rounded-b-xl py-4 ">
+          <div className="flex justify-between">
+            <h1 className="font-RubikBold ">{props.username}</h1>
+            <h1 className="font-bold font-RubikBold">Price</h1>
+          </div>
+          <div className="flex  justify-between font-mono">
+            <p>#345</p>
+            <p>0.01</p>
+          </div>
+        </div>
+      </div>
+        </>
+    )
+}
+
+export default Card
