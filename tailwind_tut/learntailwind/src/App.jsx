5250c90fd56d7990fadc7c7777f7eb0f605391d1
Dhanraj Jadhav <dj004786@gmail.com>
2024-12-18 11:57:18 +0530

    custonm_cli

---
+++ tailwind_tut/learntailwind/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-slate-700 max-h-60 w-48'></div>
+    </>
+  )
+}
+
+export default App
