f02236921cca5964c59e8d045d0f81241d531309
Dhanraj Jadhav <dj004786@gmail.com>
2024-12-27 13:21:26 +0530

    jwt addition

--- Node_practice/express/auth/index.js
+++ Node_practice/express/auth/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require("jsonwebtoken")
+const JWT_SECRET = "USER_APP";
 const app = express();
 const port = 3000
 
@@ -43,38 +45,36 @@ app.post("/signup",(req,res) => {
 
 
 //user is getting the token
-app.post("/signin",(req,res) => {
+app.post("/signin", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    const founduser = users.find(user => {
-        if(user.username == username && user.password == password) {
-            return true;
-        }else {
-            return false;
-        }
-    })
+    const user = users.find(user => user.username === username && user.password === password);
 
-    if(founduser) {
-        const token = generateToken();
-        founduser.token = token;
-        res.json({
-            message : token
+    if (user) {
+        const token = jwt.sign({
+            username: user.username
+        }, JWT_SECRET);
+
+        user.token = token;
+        res.send({
+            token
         })
-    }else {
+        console.log(users);
+    } else {
         res.status(403).send({
-            message : "invalide username or password"
+            message: "Invalid username or password"
         })
     }
-
-    console.log(users);
-
-})
+});
 
 
 app.get("/me",(req,res) => {
     const token = req.headers.authorization;
-    const user = users.find(user => user.token === token);
+    const userDetails = jwt.verify(token,JWT_SECRET);
+    const username = userDetails.username;
+    const user = users.find(user => user.username === username);
+
     if(user) {
         res.send({
             username : user.username,
